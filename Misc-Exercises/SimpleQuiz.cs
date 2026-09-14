c0b272c4b7ee925ea9aea84ecfeee8356fba9235
dime.azares@gmail.com <dime.azares@gmail.com>
2026-09-14 12:37:28 +0800

    - project directory updates

---
+++ Misc-Exercises/SimpleQuiz.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiscExercises
+{
+    internal class SimpleQuiz
+    {
+        public void task()
+        {
+            string[] quiz = new string[15]
+            {
+                "Is fish a cat?|False|1",
+                "Is Manila in Europe?|False|1",
+                "Is the Earth flat?|False|1",
+                "Is apple a fruit?|True|1",
+                "Is whale a mammal?|True|1",
+
+
+                "1+1 =?|2|2",
+                "4 x 4 =?|16|2",
+                "250/25 =?|10|2",
+                "80-90 =?|-10|2",
+                "-50 x 2 =?|-100|2",
+
+                "Does 15 % 4 == 3 evaluate to true in C#?|True|3",
+                "What is the output of (5 > 3) && !(10 <= 2)?|True|3",
+                "If int x = 5; what is the value of ++x + x++?|12|3",
+                "In C#, what is the value of 17 / 4 * 2?|8|3",
+                "What is the result of 2 + 3 * 4 - 6 / 2?|11|3"
+
+
+            };
+
+            Console.Write("Choose difficulty 1 (easy), 2 (medium), 3 (hard): ");
+            int difficulty = Convert.ToInt32(Console.ReadLine());
+
+            foreach (string QA in quiz)
+            {
+                string[] quizAnswer = QA.Split('|');
+
+
+                if (difficulty == 1 && quizAnswer[2] == "1")
+                {
+                    Console.Write("Easy Question - ");
+                    Console.Write($"{quizAnswer[0]}: ");
+                    string answer = Console.ReadLine();
+
+                    if (answer == quizAnswer[1])
+                    {
+                        Console.WriteLine("correct");
+                    }
+                    else
+                    {
+                        Console.WriteLine("incorrect");
+                    }
+                }
+                if (difficulty == 2 && quizAnswer[2] == "2")
+                {
+                    Console.Write("Medium Question - ");
+                    Console.Write($"{quizAnswer[0]}: ");
+                    string answer = Console.ReadLine();
+
+                    if (answer == quizAnswer[1])
+                    {
+                        Console.WriteLine("correct");
+                    }
+                    else
+                    {
+                        Console.WriteLine("incorrect");
+                    }
+
+                }
+                if (difficulty == 3 && quizAnswer[2] == "3")
+                {
+                    Console.Write("Hard Question - ");
+                    Console.Write($"{quizAnswer[0]}: ");
+                    string answer = Console.ReadLine();
+
+                    if (answer == quizAnswer[1])
+                    {
+                        Console.WriteLine("correct");
+                    }
+                    else
+                    {
+                        Console.WriteLine("incorrect");
+                    }
+
+                }
+
+            }
+        }
+    }
+}
---
+++ Misc-Exercises/StudentGradesAverage.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiscExercises
+{
+    internal class studentGradesAverage
+    {
+        public void task()
+        {
+            string[] status = new string[5]
+            {
+               "student 01|77|80|75|01",
+               "student 02|85|81|84|02",
+               "student 03|74|72|73|03",
+               "student 04|90|89|87|04",
+               "student 05|88|84|86|05"
+            };
+
+            Console.Write("Enter Student No. - ");
+            string studentNumber = Console.ReadLine();
+
+
+            foreach (string stat in status)
+            {
+                string[] data = stat.Split('|');
+                string student = data[0];
+                int english = int.Parse(data[1]);
+                int math = int.Parse(data[2]);
+                int science = int.Parse(data[3]);
+                string studentNo = data[4];
+
+                int average = computedAverage(english, math, science);
+
+
+
+
+                if (studentNumber == studentNo && studentNo == "01")
+                {
+
+                    if (average >= 75)
+                    {
+                        Console.WriteLine($"\n{student}: average = {average} passed");
+                        Console.WriteLine($"\n[english: {english}, math: {math}, science: {science}]");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\n{student}: average = {average} failed");
+                        Console.WriteLine($"\n[english: {english}, math: {math}, science: {science}]");
+                    }
+
+                }
+                if (studentNumber == studentNo && studentNo == "02")
+                {
+
+                    if (average >= 75)
+                    {
+                        Console.WriteLine($"\n{student}: average = {average} passed");
+                        Console.WriteLine($"\n[english: {english}, math: {math}, science: {science}]");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\n{student}: average = {average} failed");
+                        Console.WriteLine($"\n[english: {english}, math: {math}, science: {science}]");
+                    }
+
+
+                }
+                if (studentNumber == studentNo && studentNo == "03")
+                {
+
+                    if (average >= 75)
+                    {
+                        Console.WriteLine($"\n{student}: average = {average} passed");
+                        Console.WriteLine($"\n[english: {english}, math: {math}, science: {science}]");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\n{student}: average = {average} failed");
+                        Console.WriteLine($"\n[english: {english}, math: {math}, science: {science}]");
+                    }
+
+
+                }
+                if (studentNumber == studentNo && studentNo == "04")
+                {
+
+                    if (average >= 75)
+                    {
+                        Console.WriteLine($"\n{student}: average = {average} passed");
+                        Console.WriteLine($"\n[english: {english}, math: {math}, science: {science}]");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\n{student}: average = {average} failed");
+                        Console.WriteLine($"\n[english: {english}, math: {math}, science: {science}]");
+                    }
+
+
+                }
+                if (studentNumber == studentNo && studentNo == "05")
+                {
+
+                    if (average >= 75)
+                    {
+                        Console.WriteLine($"\n{student}: average = {average} passed");
+                        Console.WriteLine($"\n[english: {english}, math: {math}, science: {science}]");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\n{student}: average = {average} failed");
+                        Console.WriteLine($"\n[english: {english}, math: {math}, science: {science}]");
+                    }
+
+
+                }
+
+
+
+            }
+
+        }
+        public int computedAverage(int english, int math, int science)
+        {
+            int average = (english + math + science) / 3;
+
+            return average;
+        }
+    }
+}
